1133ba2c57da5278ed4e6adc322b0996a372280d
HonouredOne <jameswrightlpa@gmail.com>
2023-11-07 07:12:54 -0600

    added end-level flag and the ability to change to the next level/scene

---
+++ EndFlag.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@export_file("*.tscn") var next_scene
+
+var wind_speed = randf_range(0.5, 2.0)
+
+func _ready():
+	$AnimatedSprite2D.speed_scale = wind_speed
+
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		get_tree().change_scene_to_file(next_scene)
+
+func _process(delta):
+	$AnimatedSprite2D.play()
